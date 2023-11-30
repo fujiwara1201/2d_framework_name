67b97af2e62b7215f6fa130a342ee0e14c36bf1f
S0539 <S0539@21S0539>
2023-11-30 10:00:37 +0900

    test

--- MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -7,12 +7,11 @@ public class MergeHellController {
 		kadaiC();
 	}
 
-<<<<<<< HEAD
 	static void kadaiA() {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println("現在" + i + "回目のループです");
 		}
-=======
+
 	static void kadaiC() {
 	//ここに課題を書く。メソッド名変えてね。
 	int a = 50;
@@ -20,6 +19,5 @@ public class MergeHellController {
 	int c = a+b;
 	
 	System.out.println(c);
->>>>>>> refs/remotes/origin/master
 	}
 }
