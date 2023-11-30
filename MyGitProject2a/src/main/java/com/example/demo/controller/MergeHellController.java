c6c905b942d8621cd18d9a0ae5b86f8b5e980691
S0510 <S0510@21S0510>
2023-11-30 09:44:04 +0900

    C

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+
+	static void kadaiC() {
+	//ここに課題を書く。メソッド名変えてね。
+	int a = 50;
+	int b = 75;
+	int c = a+b;
+	
+	System.out.println(c);
+	}
+}
